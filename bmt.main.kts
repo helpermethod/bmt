eb8755e5601686411eb04571692cac5f9ba29be6
Oliver Weiler <oliver@sdkman.io>
2023-08-10 08:33:32 +0200

    feat: Automate login

--- bmt.main.kts
+++ bmt.main.kts
@@ -6,20 +6,32 @@ import com.microsoft.playwright.BrowserType
 import com.microsoft.playwright.Page
 import com.microsoft.playwright.Playwright
 import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
-import com.microsoft.playwright.options.AriaRole
+import com.microsoft.playwright.options.AriaRole.BUTTON
 import kotlin.io.path.Path
 
 Playwright.create().use { playwright ->
     val browser = playwright.chromium().launch(BrowserType.LaunchOptions().setHeadless(false).setSlowMo(500.0))
     val page = browser.newPage()
+
     page.navigate("https://www.rvk.de/login")
 
     assertThat(page).hasTitle("Login")
 
     page
-        .getByRole(AriaRole.BUTTON)
-        .getByText("Alles akzeptieren")
+        .getByRole(BUTTON, Page.GetByRoleOptions().setName("Anmelden"))
         .click()
 
-    page.screenshot(Page.ScreenshotOptions().setPath(Path("login.png")))
+    page
+        .locator("input[name='user']")
+        .fill(System.getenv("RVK_USERNAME"))
+
+    page
+        .locator("input[name='pass']")
+        .fill(System.getenv("RVK_PASSWORD"))
+
+    page
+        .getByRole(BUTTON, )
+        .click()
+
+    page.screenshot(Page.ScreenshotOptions().setPath(Path("fail.png")))
 }
